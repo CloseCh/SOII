2d1b6d5391ee3c29cc3fe423fadf6e37a256f3a3
CloseCh <chenwy2438516420@gmail.com>
2024-02-15 10:52:10 +0100

    init ficheros

---
+++ bloques.c
@@ -0,0 +1,55 @@
+#include "bloques.h"
+
+static int descriptor = 0;
+
+/*
+ * Function: bmount
+ * ----------------------------
+ *   ---
+ *
+ *   const char *camino: .
+ *
+ *   returns: .
+ */
+int bmount(const char *camino){
+    return 0;
+}
+
+/*
+ * Function: bumount
+ * ----------------------------
+ *   ---
+ *
+ *   returns: .
+ */
+int bumount(){
+
+}
+
+/*
+ * Function: bwrite
+ * ----------------------------
+ *   ---
+ *
+ *   unsigned int nbloque: numero de bloque.
+ *   const void *buf: Contenedor 
+ *
+ *   returns: .
+ */
+int bwrite(unsigned int nbloque, const void *buf){
+
+}
+
+/*
+ * Function: bread
+ * ----------------------------
+ *   ---
+ *
+ *   unsigned int nbloque: .
+ *   void *buf: 
+ *
+ *   returns: .
+ */
+int bread(unsigned int nbloque, void *buf){
+
+}
