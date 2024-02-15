1130a08e74215b4efb70c8fac51af29527eb4fea
Jaume Calafat <jaumecalafat123@gmail.com>
2024-02-15 11:22:48 +0100

    main cambiao

--- mi_mkfs.c
+++ mi_mkfs.c
@@ -3,5 +3,15 @@
 
 
 int main(int argc, char **argv){
-
+    //montar el dispositivo virtual
+    bmount(argv[1]);
+    //inicializar a 0s el fichero usado como dispositivo virtual
+    int nbloques=atoi(argv[2]);
+    unsigned char buf[BLOCKSIZE];
+    memset(buf,'\0',sizeof(char)); // '\0' o '0'
+    for(int i=0;i<nbloques;i++){
+        bwrite(i,buf); //escribir el bloque en el buffer
+    }
+    //desmontar el dispositivo virtual
+    bumount();
 }
