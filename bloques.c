df1399239bfd371bdc2c6e74f2a1670017b87c7a
Weiyu <chenwy2438516420@gmail.com>
2024-02-21 23:32:37 +0100

    Actualizacion para eliminar errores semana 1

--- bloques.c
+++ bloques.c
@@ -5,65 +5,91 @@ static int descriptor = 0;
 /*
  * Function: bmount
  * ----------------------------
- *   ---
+ *   Función para montar el dispositivo virtual, dado que se consta de un 
+ *   fichero, esta funcion consiste en abrir lo.
  *
- *   const char *camino: .
+ *   const char *camino: puntero a un array de tipo char con la localización 
+ *   del fichero.
  *
- *   returns: .
+ *   returns: descriptor del fichero si todo va bien, -1 si error.
  */
 int bmount(const char *camino){
-
+    //No aparecen permisos desenmascarar
+    umask(000);
     //open(camino,oflags,mode)
-    return open(camino,O_RDWR,0666);
+    descriptor = open(camino, O_RDWR | O_CREAT, 0666);
+    if (descriptor == -1) {          
+        perror(RED "Error"); printf(RESET);
+        return FALLO;
+    }
+
+    return descriptor;
 }
 
 /*
  * Function: bumount
  * ----------------------------
- *   ---
+ *   Llamar a la función close() para liberar el espacio ocupado por el 
+ *   descriptor de fichero.
  *
- *   returns: .
+ *   returns: 0 si ha ido todo bien, -1 si error.
  */
 int bumount(){
-    
     return close(descriptor);
 }
 
 /*
  * Function: bwrite
  * ----------------------------
- *   ---
+ *   Escribe 1 bloque en el dispositivo virtual, en el bloque físico 
+ *   especificado por nbloque.
  *
- *   unsigned int nbloque: numero de bloque.
- *   const void *buf: Contenedor 
+ *   unsigned int nbloque: indica la posicion del dispositivo virtual a 
+ *   introducir el contenido.
+ *   const void *buf: puntero hacia el contenido de un buffer de memoria a
+ *   volcar. 
  *
- *   returns: .
+ *   returns: tamaño del bloque si todo va bien, -1 si error.
  */
 int bwrite(unsigned int nbloque, const void *buf){
-    //lseek(descriptor, desp, origen)
-    // movemos el puntero del fichero en el offset correcto
-    off_t lseek(descriptor,nbloque*BLOCKSIZE,nbloque);
-    //volcamos el contenido del buffer en la posicion del dv
-    //write(descrptor, buffer, bytes)
-    return size_t write(descriptor,buf,BLOCKSIZE);
+    //calculamos desplazamiento
+    int desplazamiento = nbloque * BLOCKSIZE;
 
+    //movemos el puntero del fichero en el offset correcto
+    lseek(descriptor, desplazamiento, nbloque);
+
+    //volcamos el contenido del buffer en la posicion del dv
+    if (write(descriptor, buf, BLOCKSIZE) != BLOCKSIZE){
+        perror(RED "Error"); printf(RESET);
+        return FALLO;
+    }
+    return BLOCKSIZE;
 }
 
 /*
  * Function: bread
  * ----------------------------
- *   ---
+ *   Lee 1 bloque del dispositivo virtual, que se corresponde con el bloque
+ *   físico especificado por nbloque
+ *   
+ *   unsigned int nbloque: especifica el bloque a realizar la lectura.
+ * 
+ *   void *buf: puntero hacia los nbytes (BLOCKSIZE) contenidos a partir de 
+ *   la posición (nº de byte) del dispositivo virtual correspondiente al nº de 
+ *   bloque. Este contenido es el que se vuelca en un buffer de memoria.
  *
- *   unsigned int nbloque: .
- *   void *buf: 
- *
- *   returns: .
+ *   returns: tamaño del bloque si todo va bien, -1 si error.
  */
 int bread(unsigned int nbloque, void *buf){
+    //calculamos desplazamiento
+    int desplazamiento = nbloque * BLOCKSIZE;
+
     // movemos el puntero del fichero en el offset correcto
-    off_t lseek(descriptor,nbloque*BLOCKSIZE,nbloque);
-    //igual que el write
-    return size_t read(descriptor,buf,BLOCKSIZE);
+    lseek(descriptor, desplazamiento, nbloque);
 
-
+    if (read(descriptor, buf, BLOCKSIZE) != BLOCKSIZE){
+        perror(RED "Error"); printf(RESET);
+        return FALLO;
+    }
+    return BLOCKSIZE;
 }
--- mi_mkfs.c
+++ mi_mkfs.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include "bloques.h"
 
 
 int main(int argc, char **argv){
-
     //montar el dispositivo virtual
     bmount(argv[1]);
+
     //inicializar a 0s el fichero usado como dispositivo virtual
-    int nbloques=atoi(argv[2]);
+    int nbloques = atoi(argv[2]);
+
     unsigned char buf[BLOCKSIZE];
-    memset(buf,'\0',sizeof(char)); // '\0' o '0'
-    for(int i=0;i<nbloques;i++){
-        bwrite(i,buf); //escribir el bloque en el buffer
+
+    memset(buf, '\0', sizeof(char)); // '\0' o '0'
+
+    for(int i = 0; i < nbloques-2; i++){
+        bwrite(i, buf); //escribir el bloque en el buffer
     }
+
     //desmontar el dispositivo virtual
     bumount();
 }
