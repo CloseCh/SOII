d3fd0a5edd184cd9405fd7a5e8f56142277f2a46
Weiyu <chenwy2438516420@gmail.com>
2024-02-25 15:25:34 +0100

    Inicializado nivel 2

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -1,1 +1,64 @@
 #include "ficheros_basico.h"
+
+/*
+ * Function: tamMB
+ * ----------------------------
+ *   ---
+ *   
+ *   unsigned int nbloques: ---
+ *
+ *   returns: ---
+ */
+int tamMB(unsigned int nbloques){
+
+}
+
+/*
+ * Function: tamAI
+ * ----------------------------
+ *   ---
+ *   
+ *   unsigned int ninodos: ---
+ *
+ *   returns: ---
+ */
+int tamAI(unsigned int ninodos){
+
+}
+
+/*
+ * Function: initSB
+ * ----------------------------
+ *   ---
+ *   
+ *   unsigned int nbloques: ---
+ *  
+ *   unsigned int ninodos: ---
+ *
+ *   returns: ---
+ */
+int initSB(unsigned int nbloques, unsigned int ninodos){
+
+}
+
+/*
+ * Function: initMB
+ * ----------------------------
+ *   ---
+ *
+ *   returns: ---
+ */
+int initMB(){
+
+}
+
+/*
+ * Function: initAI
+ * ----------------------------
+ *   ---
+ *
+ *   returns: ---
+ */
+int initAI(){
+
+}
