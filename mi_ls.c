b07f8272a0318d63a699f127d284523ee2751a92
Weiyu <chenwy2438516420@gmail.com>
2024-04-20 00:21:04 +0200

    Introducido nivel

---
+++ mi_ls.c
@@ -0,0 +1,9 @@
+#include "directorios.h"
+
+#define TAMFILA 100
+#define TAMBUFFER (TAMFILA*1000) //suponemos un máx de 1000 entradas, aunque debería ser SB.totInodos
+
+int main(int argc, char **argv){
+
+
+}
