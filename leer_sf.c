c12eccffe1a8f146f1d0d3463b85c111aac936c2
Weiyu <chenwy2438516420@gmail.com>
2024-05-01 00:16:14 +0200

    Finalizado nivel 7

--- leer_sf.c
+++ leer_sf.c
@@ -9,17 +9,15 @@ int main(int argc, char **argv){
     switch (nivel)
     {
     case 2:
-        pruebaN2();
-        break;
+        pruebaN2(); break;
     case 3:
-        pruebaN3();
-        break;
+        pruebaN3(); break;
     case 4:
-        pruebaN4();
-        break;
+        pruebaN4(); break;
     case 5:
-        pruebaN5();
-        break;
+        pruebaN5(); break;
+    case 7:
+        pruebaN7(); break;
     default:
         printf (RED"Nivel no implementada.\n"RESET);
         break;
