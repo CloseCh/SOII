08d20f9105904f1c2876b9b8eb4321b198453031
Weiyu <chenwy2438516420@gmail.com>
2024-05-26 20:24:06 +0200

    Actualizado test12.sh

--- test12.sh
+++ test12.sh
@@ -7,3 +7,6 @@ echo -e "\x1B[38;2;17;245;120m$ ./mi_mkfs disco 100000\x1b[0m"
 echo
 echo -e "\x1B[38;2;17;245;120m$ Realizando simulacion\x1b[0m"
 time ./simulacion disco
+echo
+echo -e "\x1B[38;2;17;245;120m$ Realizando verificacion\x1b[0m"
+time ./verificacion disco
